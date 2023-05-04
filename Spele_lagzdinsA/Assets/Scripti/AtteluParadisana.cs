596a026ced8c4a6a2663cb4ae822a161ce9d9404
Alberts Lagzdins <albertslagzdins1@gmail.com>
2023-05-04 15:08:39 +0300

    Commit 1- Pievienots inputfieldi, sizi bari un dropdown box

---
+++ Spele_lagzdinsA/Assets/Scripti/AtteluParadisana.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AtteluParadisana : MonoBehaviour {
+    public Sprite goblinSlayer;
+    public Sprite goblin;
+    public GameObject mainigaisAttels;
+    public GameObject vertSlaideris;
+	public GameObject horizSlaideris;
+    public void izkritosais(int skaitlis)
+    {
+        if (skaitlis == 0){
+			mainigaisAttels.GetComponent<Image>().sprite =
+                goblinSlayer;
+		}
+        if(skaitlis == 1){
+		 mainigaisAttels.GetComponent<Image>().sprite =
+               goblin;	 
+		}
+    }
+
+    public void mainitIzmeru()
+    {
+		float pasreizejaisIzmersY =
+			vertSlaideris.GetComponent<Slider>().value;
+		float pasreizejaisIzmersX =
+			horizSlaideris.GetComponent<Slider>().value;
+		mainigaisAttels.transform.localScale =
+			new Vector2(1f* pasreizejaisIzmersX,pasreizejaisIzmersY);
+
+		mainigaisAttels.transform.localScale =
+			new Vector2(pasreizejaisIzmersX,
+				1f*pasreizejaisIzmersY);
+    }
+}
